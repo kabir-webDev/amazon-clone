6e79c1c8914c4ab9dc3e59cec8d438976d89e136
kabir-webDev <kabir.webdev@gmail.com>
2020-09-09 20:38:09 +0600

    Final work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from "react";
-import "./App.css";
-import Header from "./Header";
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Checkout from './Checkout';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <div className='app'>
+        {/* <h1>Amazon clone</h1> */}
+        <Header />
+        <Switch>
+
+          <Route path='/checkout'>
+            <Checkout />
+          </Route>
+
+          <Route path='/'>
+            <Home />
+          </Route>
+
+        </Switch>
+
+      </div>
+    </Router>
+
+
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCUHipAVaj2NFI3cyGMHU4wJepa9ui1pGw",
-  authDomain: "dot-com-35ebe.firebaseapp.com",
-  databaseURL: "https://dot-com-35ebe.firebaseio.com",
-  projectId: "dot-com-35ebe",
-  storageBucket: "dot-com-35ebe.appspot.com",
-  messagingSenderId: "1050964801660",
-  appId: "1:1050964801660:web:3aa444fc56a970236831f9",
-};
+    apiKey: "AIzaSyDS_HpucCJkShqkghn4vNXa07o_9Hvgz8M",
+    authDomain: "clone-6de06.firebaseapp.com",
+    databaseURL: "https://clone-6de06.firebaseio.com",
+    projectId: "clone-6de06",
+    storageBucket: "clone-6de06.appspot.com",
+    messagingSenderId: "86666838512",
+    appId: "1:86666838512:web:1008eb1853a1550acc1d35",
+    measurementId: "G-D7L521ZRTQ"
+  };
