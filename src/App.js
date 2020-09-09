001b67506a2a662413e023e35be3d0a21297f1dc
kabir-webDev <kabir.webdev@gmail.com>
2020-09-09 21:02:55 +0600

    Done with products

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import Home from './Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Checkout from './Checkout';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
+import Footer from "./Footer";
 
 function App() {
   return (
     <Router>
-      <div className='app'>
+      <div className="app">
         {/* <h1>Amazon clone</h1> */}
         <Header />
         <Switch>
-
-          <Route path='/checkout'>
+          <Route path="/checkout">
             <Checkout />
           </Route>
 
-          <Route path='/'>
+          <Route path="/">
             <Home />
           </Route>
-
         </Switch>
-
+        <Footer />
       </div>
     </Router>
-
-
   );
 }
 
